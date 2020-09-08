04a56111bf3cecbe9949f7c7c1412cafda10304c
Alberto-D <albertodelgado2000@gmail.com>
2020-09-08 13:30:44 +0200

    Fire comprobation  + time added

--- server.c
+++ server.c
@@ -1,5 +1,3 @@
-
-// Server side C/C++ program to demonstrate Socket programming
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -8,20 +6,30 @@
 #include <string.h>
 #define PORT 1234
 
-int clean_buffer(char buffer[]){
+void clean_buffer(char buffer[]){
 	int i = 0;
-
-	while(buffer[i] != 0){
+	while((buffer[i] != 0)&&(i<1024)){
 		buffer[i] = 0;
 		i++;
 	}
-
-
 	printf("-----------------------------\n");
-
-	return 0;
 }
 
+int check_fire(char buffer[]){
+	int fire = 0;
+	char temporal[1024];
+	strcpy(temporal, buffer);
+	char* token = strtok(temporal, " ");
+
+  	if ((buffer[0]=='f')&&(buffer[1]=='i')&&(buffer[2]=='r')&&(buffer[3]=='e')){
+		token = strtok(NULL," ");
+		if (token !=NULL){
+			printf("El token es %s \n", token);
+			fire = atoi(token);
+		}
+	}
+	return fire;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -30,7 +38,7 @@ int main(int argc, char const *argv[])
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
+	int seconds;
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -38,10 +46,8 @@ int main(int argc, char const *argv[])
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-
     // Forcefully attaching socket to the port 1234
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -49,10 +55,8 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
+    // Forcefully attaching socket to the port 1234
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -62,19 +66,25 @@ int main(int argc, char const *argv[])
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-					   (socklen_t*)&addrlen))<0)
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
 	{
 		perror("accept");
 		exit(EXIT_FAILURE);
 	}
-	while(buffer[0]!='X'){
+	while(buffer[0] != 'X'){
 		clean_buffer(buffer);
 	    valread = read( new_socket , buffer, 1024);
 	    printf("El buffer es : %s \n",buffer);
-	    //send(new_socket , hello , strlen(hello) , 0 );
-		if (buffer[0]=='a')
+		if (seconds = check_fire(buffer)){
+			printf("FIRE IN THE HOLEEEE FOR %d MILISECONDS\n",seconds);
+		}
+
+		if (buffer[0] == 'a')
 			printf("Es a\n");
+		if (buffer[0] == 0){
+			printf("Es 0\n");
+			return 0;
+		}
 	}
 
     printf("Hello message sent\n");
